50836c5eaab412dcc55ce6537a1e329806735f64
Louis Trottier <ltrottie@cmhc-schl.gc.ca>
2021-04-11 15:00:52 -0400

    Updated comments in section 1, program Bikeshare_LouisT02.py

--- bikeshare_LouisT02.py
+++ bikeshare_LouisT02.py
@@ -14,9 +14,10 @@ WKDAYS = ['all','sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday'
 
 def get_filters():
     """
-    Asks user to specify a city, month, and day to analyze.
+    Here are the steps performed by the Python program
+    1-Asks user to specify a city, month, and day to analyze.
 
-    Returns:
+    2-Returns:
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
