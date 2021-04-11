d3a10040e8ad73c4067238ad543a4883e99d00dd
Louis Trottier <ltrottie@cmhc-schl.gc.ca>
2021-04-11 16:50:23 -0400

    Refactoring: Turning 2 statements into a single one to handle city name input

--- bikeshare_LouisT02.py
+++ bikeshare_LouisT02.py
@@ -24,8 +24,7 @@ def get_filters():
     print('Hello! Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while True:    
-        print('Which city do you want to analyze?  (Chicago, New York City, Washington)')
-        city = input("Enter name: ").lower()
+        city = input("Which city do you want to analyze?  (Chicago, New York City, Washington)  Enter name: ").lower()
         if city not in CITIES:
             print("\nInvalid Answer\n")
             continue
