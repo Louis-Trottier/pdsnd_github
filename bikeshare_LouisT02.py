26a279da357cec2da3e18657d4905ae29e6e97a5
Louis Trottier <ltrottie@cmhc-schl.gc.ca>
2021-04-11 15:04:52 -0400

    Updated comments, program Bikeshare_LouisT02.py

--- bikeshare_LouisT02.py
+++ bikeshare_LouisT02.py
@@ -17,13 +17,13 @@ def get_filters():
     Here are the steps performed by the Python program
     1-Asks user to specify a city, month, and day to analyze.
 
-    2-Returns:
+    2-What does the program return?
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # TO DO: get user input for city (chicago, new york city, washington).
     while True:    
         print('Which city do you want to analyze?  (Chicago, New York City, Washington)')
         city = input("Enter name: ").lower()
